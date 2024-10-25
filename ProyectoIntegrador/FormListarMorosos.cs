d580a8c7dcf5b823e68758dc5080d7c09a9e0e38
Facundo Azcue <fazcue@gmail.com>
2024-10-25 06:13:42 -0300

    Agregado botones Actualizar y Volver

--- ProyectoIntegrador/FormListarMorosos.cs
+++ ProyectoIntegrador/FormListarMorosos.cs
@@ -20,26 +20,46 @@ namespace ProyectoIntegrador
 
         private void CargaGrilla()
         {
+            // consultar listado de morosos
             List<E_Socio> morosos = Socio.ListarMorosos();
 
             if (morosos.Count > 0)
             {
-                MessageBox.Show("Hay morosos");
-
                 for (int i = 0; i < morosos.Count; i++)
                 {
+                    // nuevo renglón
                     dtgvMorosos.Rows.Add();
 
+                    // carga de datos
                     dtgvMorosos.Rows[i].Cells[0].Value = morosos[i].Nombre;
                     dtgvMorosos.Rows[i].Cells[1].Value = morosos[i].Apellido;
                     dtgvMorosos.Rows[i].Cells[2].Value = morosos[i].Dni;
                     dtgvMorosos.Rows[i].Cells[3].Value = morosos[i].Fecha_vencimiento_cuota.ToString("dd-MM-yyyy");
                 }
+
+                // visibilidad de elementos
+                lblMensajeAlDia.Visible = false;
+                dtgvMorosos.Visible = true;
             }
             else
             {
-                MessageBox.Show("NO Hay morosos");
+                // visibilidad de elementos
+                dtgvMorosos.Visible = false;
+                lblMensajeAlDia.Visible = true;
             }
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form menuPrincipal = new FrmMenuPrincipal();
+            menuPrincipal.Show();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            dtgvMorosos.Rows.Clear();
+            CargaGrilla();
         }
     }
 }
