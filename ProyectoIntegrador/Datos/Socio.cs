17e8548c4cf05e5fa42f789fffd062d4e7252688
Facundo Azcue <fazcue@gmail.com>
2024-11-10 22:54:30 -0300

    Actualizado query ListarMorosos / Buscar / Añadido PagarCuota

--- ProyectoIntegrador/Datos/Socio.cs
+++ ProyectoIntegrador/Datos/Socio.cs
@@ -14,17 +14,19 @@ namespace ProyectoIntegrador.Datos
             List<E_Socio> morosos = new List<E_Socio>();
 
             MySqlDataReader respuesta;
+            DateTime fechaActual = DateTime.Now;
 
             try
             {
                 sqlCon = Conexion.Crear();
 
                 // query
-                string query = "SELECT c.nombre, c.apellido, c.dni, s.nro_carnet, s.fecha_vencimiento_cuota FROM cliente c JOIN socio s ON s.id = c.id WHERE estado = 'inactivo';";
+                string query = "SELECT c.nombre, c.apellido, c.dni, s.nro_carnet, c.fecha_vencimiento_cuota FROM cliente c JOIN socio s ON s.id_cliente = c.id WHERE c.fecha_vencimiento_cuota <= @fechaActual;";
 
                 // comando de tipo texto utilizando la query definida
                 MySqlCommand comando = new MySqlCommand(query, sqlCon);
                 comando.CommandType = CommandType.Text;
+                comando.Parameters.AddWithValue("@fechaActual", fechaActual);
 
                 // abrimos la conexion
                 sqlCon.Open();
@@ -61,18 +63,18 @@ namespace ProyectoIntegrador.Datos
             return morosos;
         }
 
-        public static E_Socio BuscarSocio(string dni)
+        public static E_Socio Buscar(string dni)
         {
             MySqlConnection sqlCon = new MySqlConnection();
-            E_Socio socioEncontrado = null;
+            E_Socio encontrado = null;
 
             try
             {
                 sqlCon = Conexion.Crear();
                 // query para buscar al socio por su dni
-                string query = "SELECT c.nombre, c.apellido, c.dni, s.nro_carnet, s.fecha_vencimiento_cuota " +
+                string query = "SELECT c.nombre, c.apellido, c.dni, s.nro_carnet, c.fecha_vencimiento_cuota " +
                                "FROM cliente c JOIN socio s ON s.id_cliente = c.id " +
-                               "WHERE c.dni = @dni AND c.estado = 'activo';";
+                               "WHERE c.dni = @dni;";
 
                 MySqlCommand comando = new MySqlCommand(query, sqlCon);
                 comando.CommandType = CommandType.Text;
@@ -90,7 +92,7 @@ namespace ProyectoIntegrador.Datos
                     DateTime fecha_vencimiento_cuota = (DateTime)respuesta["fecha_vencimiento_cuota"];
 
                     // nueva instancia de E_Socio con los datos obtenidos
-                    socioEncontrado = new E_Socio(nombre, apellido, dni, nro_carnet, fecha_vencimiento_cuota);
+                    encontrado = new E_Socio(nombre, apellido, dni, nro_carnet, fecha_vencimiento_cuota);
                 }
             }
             catch (Exception ex)
@@ -102,7 +104,48 @@ namespace ProyectoIntegrador.Datos
                 if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
             }
 
-            return socioEncontrado;
+            return encontrado;
+        }
+
+        public static bool PagarCuota(string dni, string nuevoVencimiento)
+        {
+            MySqlConnection sqlCon = new MySqlConnection();
+            MySqlDataReader respuesta;
+
+            try
+            {
+                sqlCon = Conexion.Crear();
+
+                // query
+                string query = "UPDATE cliente c JOIN socio s ON s.id_cliente = c.id SET fecha_vencimiento_cuota = @nuevoVencimiento WHERE c.dni = @dni;";
+
+                // comando de tipo texto utilizando la query definida
+                MySqlCommand comando = new MySqlCommand(query, sqlCon);
+                comando.CommandType = CommandType.Text;
+                comando.Parameters.AddWithValue("@dni", dni);
+                comando.Parameters.AddWithValue("@nuevoVencimiento", nuevoVencimiento);
+
+                // abrimos la conexion
+                sqlCon.Open();
+
+                // almacenamos el resulatdo en la variable
+                respuesta = comando.ExecuteReader();
+
+                if (respuesta.HasRows)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al pagar cuota.", ex);
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open) sqlCon.Close();
+            }
+
+            return false;
         }
     }
 }
