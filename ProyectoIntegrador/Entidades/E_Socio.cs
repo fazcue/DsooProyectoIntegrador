c1da3ab55d41cba1e9f522bd54b99a8508c472ee
Facundo Azcue <fazcue@gmail.com>
2024-11-10 22:57:11 -0300

    Eliminado atributo fecha_vencimiento_cuota

--- ProyectoIntegrador/Entidades/E_Socio.cs
+++ ProyectoIntegrador/Entidades/E_Socio.cs
@@ -5,15 +5,13 @@ namespace ProyectoIntegrador.Entidades
     internal class E_Socio : E_Cliente
     {
         private int nro_carnet;
-        private DateTime fecha_vencimiento_cuota;
 
-        public E_Socio(string nombre, string apellido, string dni, int nro_carnet, DateTime fecha_vencimiento_cuota, string estado = "inactivo") : base(nombre, apellido, dni, estado)
+        public E_Socio(string nombre, string apellido, string dni, int nro_carnet, DateTime fecha_vencimiento_cuota)
+            : base(nombre, apellido, dni, fecha_vencimiento_cuota)
         {
             this.nro_carnet = nro_carnet;
-            this.fecha_vencimiento_cuota = fecha_vencimiento_cuota;
         }
 
         public int Nro_carnet { get { return nro_carnet; } }
-        public DateTime Fecha_vencimiento_cuota { get { return fecha_vencimiento_cuota; } }
     }
 }
