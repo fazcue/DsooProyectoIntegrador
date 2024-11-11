f95730c4f0a69aded63bfe9c3a2efcb9cad50288
Facundo Azcue <fazcue@gmail.com>
2024-11-10 22:56:28 -0300

    Añadido clase entidad E_NoSocio

---
+++ ProyectoIntegrador/Entidades/E_NoSocio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoIntegrador.Entidades
+{
+    internal class E_NoSocio : E_Cliente
+    {
+        public E_NoSocio(string nombre, string apellido, string dni, DateTime fecha_vencimiento_cuota)
+            : base(nombre, apellido, dni, fecha_vencimiento_cuota)
+        {
+        }
+    }
+}
